3bbda0c518b0a14e9ab3afb6b069def47b1d260f
Andrew <rainbowshardofdeath@gmail.com>
2021-09-05 21:06:00 +0400

    Add health for player

---
+++ Assets/Scripts/Mechanics/Health.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mechanics
+{
+    public class Health : MonoBehaviour
+    {
+        public float maxHp = 5;
+
+        public delegate void MethodContainer();
+        public event MethodContainer Death;
+        
+        private float _currentHp;
+
+        private void Awake() => _currentHp = maxHp;
+
+        public void Damage()
+        {
+            --_currentHp;
+            Debug.Log($"Current hp: {_currentHp}");
+            if (IsDead())
+                Death?.Invoke();
+        }
+
+        private bool IsDead() => _currentHp < 0.01;
+
+        public void Heal()
+            => _currentHp = maxHp;
+    }
+}
