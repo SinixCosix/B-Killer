da9c8fba1a8f9b17854d349110d38a0aaca11a5a
Andrew <rainbowshardofdeath@gmail.com>
2021-09-09 22:35:45 +0400

    Add primitive shooting

---
+++ Assets/Scripts/Gameplay/Bullet/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Bullet
+{
+    public class Bullet : MonoBehaviour
+    {
+        public float speed = 20f;
+        public Rigidbody2D rb;
+
+        private void Update()
+        {
+            rb.velocity = transform.up * speed;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log(other.name);
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Bullet/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Bullet
+{
+    public class Weapon : MonoBehaviour
+    {
+        public Transform firePoint;
+        public GameObject bulletPrefab;
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+                Shoot();
+        }
+
+        private void Shoot()
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            
+        }
+    }
+}
