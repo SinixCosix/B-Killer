908d77d444382f1a6bd7767536d5dc211b7a5358
Andrew <rainbowshardofdeath@gmail.com>
2021-10-08 20:20:03 +0400

    Add primitive generation

--- Assets/Scripts/Mechanics/BlockSpawner.cs
+++ Assets/Scripts/Mechanics/BlockSpawner.cs
@@ -2,20 +2,13 @@ using UnityEngine;
 
 namespace Mechanics
 {
-    public class SpawnBlock : MonoBehaviour
+    public class BlockSpawner : MonoBehaviour
     {
         public GameObject[] blocks;
-        // Start is called before the first frame update
         private void Start()
         {
             var id = Random.Range(0, blocks.Length);
             Instantiate(blocks[id], transform.position, Quaternion.identity);
-        }
-
-        // Update is called once per frame
-        private void Update()
-        {
-        
         }
     }
 }
---
+++ Assets/Scripts/Mechanics/LevelGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Mechanics
+{
+    public class LevelGenerator : MonoBehaviour
+    {
+        public Transform startingPosition;
+        public GameObject[] rooms;
+        public float moveAmount;
+
+        public float spawnRoomTime = 0.25f;
+        private float _spawnRoomTime;
+        
+        private int _direction;
+
+        private void Start()
+        {
+            Instantiate(rooms[0], transform.position, Quaternion.identity);
+            _direction = Random.Range(1, 6);
+        }
+
+        private void Update()
+        {
+            if (_spawnRoomTime <= 0)
+            {
+                Move();
+                _spawnRoomTime = spawnRoomTime;
+            }
+            else
+                _spawnRoomTime -= Time.deltaTime;
+        }
+
+        private void Move()
+        {
+            var position = transform.position;
+            
+            switch (_direction)
+            {
+                case 1:
+                case 2:
+                    transform.position = new Vector2(position.x + moveAmount, position.y);
+                    break;
+                case 3:
+                case 4:
+                    transform.position = new Vector2(position.x - moveAmount, position.y);
+                    break;
+                case 5:
+                    transform.position = new Vector2(position.x , position.y- moveAmount);
+                    break;
+            }
+            
+            Instantiate(rooms[0], transform.position, Quaternion.identity);
+            _direction = Random.Range(1, 6);
+        }
+    }
+}
