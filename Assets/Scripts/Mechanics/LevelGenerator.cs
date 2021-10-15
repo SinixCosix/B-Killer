8458aae8d0e866bf66105ced9356fec8cf241f56
Andrew <rainbowshardofdeath@gmail.com>
2021-10-16 01:17:33 +0400

    Add random size room

--- Assets/Scripts/Mechanics/LevelGenerator.cs
+++ Assets/Scripts/Mechanics/LevelGenerator.cs
@@ -12,6 +12,7 @@ namespace Mechanics
         public Vector2 startPoint;
         public Vector2 endPoint;
         public int roomsCount = 10;
+        public int minRoomDistance = 5;
         public GameObject sprite;
         public float time = 10;
 
@@ -60,14 +61,11 @@ private void Update()
                 return;
             }
 
-            var splitByVerticalValue = Random.Range(0, 2);
-            var splitByVertical = splitByVerticalValue == 0;
-            var splitByHorizontalValue = Random.Range(0, 2);
-            var splitByHorizontal = splitByHorizontalValue == 0;
-            Debug.Log($"vert {splitByVertical}, hor {splitByHorizontal}");
+            var splitByVertical = Random.Range(0, 2) == 0;
+            var splitByHorizontal = Random.Range(0, 2) == 0;
             
-            var width = rect.width / 2;
-            var height = rect.height / 2;
+            var width = splitByVertical ? rect.width / 2 : rect.width;
+            var height = splitByHorizontal ? rect.height / 2 : rect.height;
 
             var x1 = splitByVertical ? rect.x + width : rect.x;
             var y1 = splitByHorizontal ? rect.y + height : rect.y;
@@ -75,7 +73,7 @@ private void Update()
             var parts1 = Random.Range(1, parts);
 
             var x2 = splitByVertical ? x1 - width : x1 + width;
-            var y2 = splitByHorizontal ? y1 - width : y1 + width;
+            var y2 = splitByHorizontal ? y1 - height : y1 + height;
             var rect2 = new Rect(x2, y2, width, height);
             var parts2 = parts - parts1;
 
@@ -89,9 +87,12 @@ private void Update()
         {
             foreach (var room in _rooms)
             {
+                var width = Random.Range(room.width / 2 + minRoomDistance, room.width - minRoomDistance);
+                var height = Random.Range(room.height / 2+ minRoomDistance, room.height - minRoomDistance);
                 var position = new Vector2(room.x, room.y);
+                
                 var newSprite = Instantiate(sprite, position, Quaternion.identity);
-                newSprite.transform.localScale = new Vector3(room.width, room.height);
+                newSprite.transform.localScale = new Vector3(width, height);
                 _sprites.Add(newSprite);
             }
         }
