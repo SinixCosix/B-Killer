cc039bfa6b0685f51cd30b1af0c573af775ce149
Andrew <rainbowshardofdeath@gmail.com>
2022-01-09 15:37:13 +0400

    Update room room generation

--- Assets/Scripts/Mechanics/MapGeneration/LawnsGenerator.cs
+++ Assets/Scripts/Mechanics/MapGeneration/LawnsGenerator.cs
@@ -46,15 +46,43 @@ namespace Mechanics.MapGeneration
         private static void GeneratePoints(IEnumerable<Rect> rooms)
         {
             var pointsOfLawns = MapGenerator.Instance.PointsOfLawns;
-            foreach (var room in rooms)
+            foreach (var rect in rooms)
             {
-                for (var i = room.x; i < room.xMax; ++i)
-                for (var j = room.y; j < room.yMax; ++j)
-                {
-                    var position = new Vector2Int((int) i, (int) j);
-                    pointsOfLawns.Add(position);
-                }
+                var ellipsePoints = GenerateEllipse(rect);
+
+                foreach (var point in ellipsePoints)
+                    pointsOfLawns.Add(point);
             }
+        }
+
+        private static HashSet<Vector2Int> GenerateEllipse(Rect rect)
+        {
+            var points = new HashSet<Vector2Int>();
+            const float increment = 0.5f;
+            for (var x = rect.x; x < rect.xMax; x += increment)
+            for (var y = rect.y; y < rect.yMax; y += increment)
+            {
+                var result = CalculateEllipsePoint(
+                    x, y,
+                    rect.center.x, rect.center.y,
+                    rect.width/ 2, rect.height / 2);
+
+                if (result < 1)
+                    points.Add(new Vector2Int((int) x, (int) y));
+            }
+
+            return points;
+        }
+
+        private static float CalculateEllipsePoint(
+            float x, float y,
+            float x0, float y0,
+            float a, float b)
+        {
+            var term1 = Mathf.Pow(x - x0, 2) / (a * a);
+            var term2 = Mathf.Pow(y - y0, 2) / (b * b);
+
+            return term1 + term2;
         }
     }
 }
